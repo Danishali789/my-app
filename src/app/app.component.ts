6fbece52fc659157b76aa1636a0791a4ea8588e9
Danish Margoob <danish876a@gmail.com>
2023-04-11 18:03:25 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import{FormGroup,FormBuilder,Validators} from '@angular/forms'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'my-app';
+  registration:FormGroup;
+
+  constructor(private re:FormBuilder){
+    this.registration=this.re.group({
+      fname:['',[Validators.required, Validators.pattern('^[a-zA-Z ]*$')]],
+      lname:['',[Validators.required, Validators.pattern('^[a-zA-Z ]*$')]]
+    })
+  }
+  // onSubmit(){
+  //   console.log(this.registration);
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BorderComponent } from './border/border.component';
+
+import { ReactiveFormsModule } from '@angular/forms';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BorderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+  TabsModule.forRoot(),
+    AppRoutingModule,
+    BsDropdownModule.forRoot(),
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
