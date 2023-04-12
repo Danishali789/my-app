3e06a2e6fcd11ed67c0b5f8697b4bca79a054967
Danish Margoob <danish876a@gmail.com>
2023-04-12 16:55:55 +0530

    apply custom validation for confirm pssword

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,OnInit} from '@angular/core';
 import{FormGroup,FormBuilder,Validators} from '@angular/forms'
 
 @Component({
@@ -7,17 +7,42 @@ import{FormGroup,FormBuilder,Validators} from '@angular/forms'
   styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-app';
-  registration:FormGroup;
+  registration:any=FormGroup;
 
-  constructor(private re:FormBuilder){
-    this.registration=this.re.group({
-      fname:['',[Validators.required, Validators.pattern('^[a-zA-Z ]*$')]],
-      lname:['',[Validators.required, Validators.pattern('^[a-zA-Z ]*$')]]
-    })
+  constructor(private re:FormBuilder){}
+
+    ngOnInit(): void {
+      this.registration=this.re.group({
+        fname:['',[Validators.required, Validators.pattern('^[a-zA-Z ]*$'),Validators.minLength(3)]],
+        lname:['',[Validators.required, Validators.pattern('^[a-zA-Z ]*$')]],
+        email:['',[Validators.required,Validators.email]],
+        password:['',[Validators.required,Validators.minLength(8)]],
+        repass:['',[Validators.required]]
+      },
+      {
+        validators:this.mustMatch('password','repass')
+      }
+      )
+    }
+    
+  mustMatch(password:any,repass:any){
+    return(fg:FormGroup)=>{
+      const passwordControl=fg.controls[password];
+      const cnfpasswordControl=fg.controls[repass];
+      if(cnfpasswordControl.errors && !cnfpasswordControl.errors['mustMatch']){
+         return;
+      }
+      if(passwordControl.value !== cnfpasswordControl.value){ 
+        cnfpasswordControl.setErrors({mustMatch:true});
+      }else{
+        cnfpasswordControl.setErrors(null);
+      }
+    } 
   }
-  // onSubmit(){
-  //   console.log(this.registration);
-  // }
+
+  onSubmit(){
+    console.log(this.registration);
+  }
 }
